89a157334529f9173e1dba097dd760d04d0e98b9
Miguel Gómez <145044778+MGrammer02@users.noreply.github.com>
2023-11-12 12:53:40 -0500

    SHEDU-Upload
    ss

---
+++ SHEDU/scripts/home.js
@@ -0,0 +1,18 @@
+
+sectionTeachers = document.getElementById("adm-teachers");
+sectionSubjects = document.getElementById("adm-subjects");
+sectionCourses = document.getElementById("adm-courses");
+sectionSheduls = document.getElementById("adm-sheduls");
+
+sectionTeachers.addEventListener("click", ()=>{
+    window.open("../admin-teachers.html");
+})
+sectionSubjects.addEventListener("click", ()=>{
+    window.open("../admin-subjects.html");
+})
+sectionCourses.addEventListener("click", ()=>{
+    window.open("../admin-courses.html");
+})
+sectionSheduls.addEventListener("click", ()=>{
+    window.open("../admin-sheduls.html");
+})
